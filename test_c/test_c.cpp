8229e40e6aaa416b1449167229c7291ae4683466
Coder <1187736527@qq.com>
2018-04-06 19:27:44 +0800

    BTree
    creat BTree

--- test_c/test_c.cpp
+++ test_c/test_c.cpp
@@ -2,9 +2,42 @@
 //
 
 #include "stdafx.h"
+#include <stdlib.h>
+
+typedef struct BTNode{
+	int data;
+	struct BTNode *lchild,*rchild;
+}BTNode,*BTree;
+
+int creatTree(BTree &T){
+	int data=0;
+	scanf("%d",&data);
+	if(data){
+		T=(BTree)malloc(sizeof(BTNode));	
+		T->data=data;
+		creatTree(T->lchild);
+		creatTree(T->rchild);
+	}
+	else{
+		T=NULL;
+	}
+	return 0;
+}
+
+void preOrder(BTree T){
+	if(T){
+		printf("%d ",T->data);
+		preOrder(T->lchild);
+		preOrder(T->rchild);
+	}
+}
 
 int main(int argc, char* argv[])
 {
+	BTree T;
+	creatTree(T);
+	preOrder(T);
+	scanf("%d");
 	printf("Hello World!\n");
 	return 0;
 }
